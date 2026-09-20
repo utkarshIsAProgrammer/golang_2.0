85128f938b33584689af011fd353fc899edbd609
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2026-09-20 21:06:00 +0530

    created web server

---
+++ projects/01_web_server/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() error %v", err)
+	}
+
+	fmt.Println(w, "POST request successful")
+	name := r.FormValue("name")
+	email := r.FormValue("email")
+	fmt.Fprintf(w, "Name: %s\n", name)
+	fmt.Fprintf(w, "Email: %s\n", email)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Only GET method is supported", http.StatusMethodNotAllowed)
+		return
+	}
+
+	fmt.Fprintln(w, "Hello!")
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Println("Server is running on port 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
